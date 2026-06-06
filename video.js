fdd15d647f02d0a51c2dc94b5f6578569b30a87c
Samuelba._. <hello@tutorapide.xyz>
2026-06-06 21:28:18 +0200

    Delete video.js

--- video.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const videoSchema = new mongoose.Schema({
-    channelId: { type: String, }, // ID de la chaîne YouTube
-    lastVideoId: { type: String, default: null }, // Par défaut, null
-});
-
-module.exports = mongoose.model('Video', videoSchema);
